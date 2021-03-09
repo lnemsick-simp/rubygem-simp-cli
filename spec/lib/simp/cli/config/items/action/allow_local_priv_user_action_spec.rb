605ffe78e7d683e84f1993d3e97fd2152198882b
lnemsick-simp <lnemsick.simp@gmail.com>
2021-03-09 15:03:57 -0500

    more tests

--- spec/lib/simp/cli/config/items/action/allow_local_priv_user_action_spec.rb
+++ spec/lib/simp/cli/config/items/action/allow_local_priv_user_action_spec.rb
@@ -2,35 +2,35 @@ require 'simp/cli/config/items/action/allow_local_priv_user_action'
 require_relative '../spec_helper'
 
 describe Simp::Cli::Config::Item::AllowLocalPrivUserAction do
-  def build_config_items(fqdn, local_priv_user)
+  def build_config_items(fqdn, local_priv_user, puppet_env_info)
     config_items = {}
-    item = Simp::Cli::Config::Item::CliNetworkHostname.new(@puppet_env_info)
+    item = Simp::Cli::Config::Item::CliNetworkHostname.new(puppet_env_info)
     item.value = fqdn
     config_items[item.key] = item
 
-    item = Simp::Cli::Config::Item::CliLocalPrivUser.new(@puppet_env_info)
+    item = Simp::Cli::Config::Item::CliLocalPrivUser.new(puppet_env_info)
     item.value = local_priv_user
     config_items[item.key] = item
 
-    item = Simp::Cli::Config::Item::CliLocalPrivUserExists.new(@puppet_env_info)
+    item = Simp::Cli::Config::Item::CliLocalPrivUserExists.new(puppet_env_info)
     item.value = true
     config_items[item.key] = item
 
-    item = Simp::Cli::Config::Item::CliLocalPrivUserHasAuthorizedSshKeys.new(@puppet_env_info)
+    item = Simp::Cli::Config::Item::CliLocalPrivUserHasAuthorizedSshKeys.new(puppet_env_info)
     item.value = true
     config_items[item.key] = item
 
-    item = Simp::Cli::Config::Item::PamAccessUsers.new(@puppet_env_info)
+    item = Simp::Cli::Config::Item::PamAccessUsers.new(puppet_env_info)
     item.config_items = config_items
     item.value = item.get_recommended_value
     config_items[item.key] = item
 
-    item = Simp::Cli::Config::Item::SelinuxLoginResources.new(@puppet_env_info)
+    item = Simp::Cli::Config::Item::SelinuxLoginResources.new(puppet_env_info)
     item.config_items = config_items
     item.value = item.get_recommended_value
     config_items[item.key] = item
 
-    item = Simp::Cli::Config::Item::SudoUserSpecifications.new(@puppet_env_info)
+    item = Simp::Cli::Config::Item::SudoUserSpecifications.new(puppet_env_info)
     item.config_items = config_items
     item.value = item.get_recommended_value
     config_items[item.key] = item
@@ -62,7 +62,7 @@ describe Simp::Cli::Config::Item::AllowLocalPrivUserAction do
 
   describe '#apply' do
     it 'adds ssh+sudo config to <host>.yaml' do
-      @ci.config_items = build_config_items(@fqdn, 'local_admin')
+      @ci.config_items = build_config_items(@fqdn, 'local_admin', @puppet_env_info)
 
       file = File.join(@files_dir, 'puppet.your.domain.yaml')
       FileUtils.copy_file file, @host_file
@@ -74,7 +74,7 @@ describe Simp::Cli::Config::Item::AllowLocalPrivUserAction do
     end
 
     it 'merges with existing ssh+sudo config in <host>.yaml' do
-      @ci.config_items = build_config_items(@fqdn, 'new_local_admin')
+      @ci.config_items = build_config_items(@fqdn, 'new_local_admin', @puppet_env_info)
 
       file = File.join(@files_dir, 'host_with_allow_local_priv_user.yaml')
       FileUtils.copy_file file, @host_file
@@ -86,14 +86,14 @@ describe Simp::Cli::Config::Item::AllowLocalPrivUserAction do
     end
 
     it 'fails when <host>.yaml does not exist' do
-      @ci.config_items = build_config_items(@fqdn, 'local_admin')
+      @ci.config_items = build_config_items(@fqdn, 'local_admin', @puppet_env_info)
 
       @ci.apply
       expect( @ci.applied_status ).to eq :failed
     end
 
     it 'fails when cli::network::hostname item does not exist' do
-      @ci.config_items = build_config_items(@fqdn, 'local_admin')
+      @ci.config_items = build_config_items(@fqdn, 'local_admin', @puppet_env_info)
       @ci.config_items.delete('cli::network::hostname')
       expect{ @ci.apply }.to raise_error( Simp::Cli::Config::MissingItemError,
         'Internal error: Simp::Cli::Config::Item::AllowLocalPrivUserAction' +
@@ -106,7 +106,7 @@ describe Simp::Cli::Config::Item::AllowLocalPrivUserAction do
      'sudo::user_specifications'
     ].each do |item_key|
       it "fails when #{item_key} item does not exist" do
-        @ci.config_items = build_config_items(@fqdn, 'local_admin')
+        @ci.config_items = build_config_items(@fqdn, 'local_admin', @puppet_env_info)
         @ci.config_items.delete(item_key)
         file = File.join(@files_dir, 'puppet.your.domain.yaml')
         FileUtils.copy_file file, @host_file
@@ -119,13 +119,13 @@ describe Simp::Cli::Config::Item::AllowLocalPrivUserAction do
 
   describe '#apply_summary' do
     it 'reports unattempted status when #apply not called' do
-      @ci.config_items = build_config_items(@fqdn, 'local_admin')
+      @ci.config_items = build_config_items(@fqdn, 'local_admin', @puppet_env_info)
       expect( @ci.apply_summary ).to eq(
         "Configuring ssh & sudo for local user 'local_admin' in SIMP server <host>.yaml unattempted")
     end
 
     it 'fails when cli::local_priv_user item does not exist' do
-      @ci.config_items = build_config_items(@fqdn, 'local_admin')
+      @ci.config_items = build_config_items(@fqdn, 'local_admin', @puppet_env_info)
       @ci.config_items.delete('cli::local_priv_user')
       expect{ @ci.apply_summary }.to raise_error( Simp::Cli::Config::MissingItemError,
         'Internal error: Simp::Cli::Config::Item::AllowLocalPrivUserAction' +
